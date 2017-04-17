cf8e04eab716735b3333a4ef23d7465a0e8f4c9d
chenkang1 <chenkang1@lenovo.com>
2017-04-17 17:53:59 +0800

    fixed auth.html css path

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ console.log("=============================" + env + "===========================
 console.log("=============================" + __dirname + "=============================");
 
 var webpackConfig = {
-    devtool: 'cheap-module-source-map', //generate source map for developing
+    devtool: 'module-source-map', //generate source map for developing
     entry: {
         app: path.join(__dirname, "/app/bootstrap/bootstrap.js"), //the main file for start app
         // vendor: [
