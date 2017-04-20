545c008b3f9c59834c160749b3f5b28f3b4720b9
chenkang1 <chenkang1@lenovo.com>
2017-04-20 16:10:33 +0800

    add html5 mode

--- app/bootstrap/app.js
+++ app/bootstrap/app.js
@@ -1,18 +1,25 @@
 'use strict';
 
 angular.module('BlurAdmin', [
-  'ngAnimate',
-  'ui.bootstrap',
-  'ui.sortable',
-  'ui.router',
-  'ngTouch',
-  'toastr',
-  'smart-table',
-  "xeditable",
-  'ui.slimscroll',
-  'ngJsTree',
-  'angular-progress-button-styles',
+        'ngAnimate',
+        'ui.bootstrap',
+        'ui.sortable',
+        'ui.router',
+        'ngTouch',
+        'toastr',
+        'smart-table',
+        "xeditable",
+        'ui.slimscroll',
+        'ngJsTree',
+        'angular-progress-button-styles',
 
-  'BlurAdmin.theme',
-  'BlurAdmin.pages'
-]);
+        'BlurAdmin.theme',
+        'BlurAdmin.pages'
+    ])
+    .config(function($locationProvider) {
+        // Set to use HTML5 mode, which removes the #! from modern browsers.
+        // Only when config it and browser support HTML5 history API
+        let isHtml5Mode = !!true && (window.history && 'pushState' in window.history);
+        $locationProvider.html5Mode(isHtml5Mode);
+        $locationProvider.hashPrefix('!');
+    })
